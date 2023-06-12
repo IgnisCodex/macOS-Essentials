f9fedc2ce44f93ee992b941a0c97388a63ee1b27
IgnisCodex <edwardgwilletts23@icloud.com>
2023-06-12 22:53:03 +0100

    Initial Commit

---
+++ macOS Essentials/macOS_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_EssentialsApp.swift
+//  macOS Essentials
+//
+//  Created by Edward Willetts on 12/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct macOS_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
